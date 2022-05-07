709679c1f02a4d3d4c59737726d67b2186727249
ujiun <ujiun@hanmail.net>
2022-05-08 06:43:39 +0900

    20번 미완성

---
+++ src/com/javaex/practice/Ex20.java
@@ -0,0 +1,15 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex20 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		sc.close();
+
+	}
+
+}
